4e521ed2a5ed39ed0763369baed973f15de2d062
Lara del Cerro <romerojimenezeulalia@gmail.com>
2026-04-16 13:22:34 +0200

    Tipos básicos

---
+++ 1_basicos/holamundo.py
@@ -0,0 +1,10 @@
+print("Hola,mundo")
+
+#Comentario en una única línea 
+""""
+Comentario 
+en varias 
+líneas
+
+"""
+
---
+++ 1_basicos/tiposbasicos.py
@@ -0,0 +1,26 @@
+nombre_alumno = "Lara del Cerro"
+print (nombre_alumno)
+print(type(nombre_alumno)) #imprime el tipo de vble 
+print("____________")
+nombre_alumno = 21
+print(nombre_alumno)
+print(type(nombre_alumno))
+
+#Tipos básicos en Python
+
+#1. Numéricos
+
+edad= 34
+precio = 1299.35
+
+#2. Booleano
+
+estado = True
+activo = False
+
+#3. Cadena de caracteres - string
+
+nombre = "Irene"
+apellidos = 'Martínez'
+mensaje = 'El niño dijo: "¿Qué pasa?"'
+print(mensaje)
