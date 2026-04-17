503aac6b497bc3306209f66260dda54c8f0ee755
Lara del Cerro <romerojimenezeulalia@gmail.com>
2026-04-17 10:41:59 +0200

    ampliación operadoresMatematicos y creación operadoresComparación

---
+++ 1_basicos/operadoresComparacion.py
@@ -0,0 +1,18 @@
+numA = 18
+numB = 5
+
+#1. Mayor que
+resultado = numA > numB #>=
+print("Mayor que:", resultado)
+
+#2. Menor que
+resultado= numA<numB #<=
+print("Menor que:", resultado)
+
+#3. Igualdad
+resultado = numA==numB
+print("Igual que :", resultado)
+
+#4. Distinto 
+resultado = numA != numB
+print("Distinto que :", resultado)
---
+++ 1_basicos/operadoresMatematicos.py
@@ -0,0 +1,32 @@
+numA = 31
+numB = 12
+
+#1. Suma
+print (5 + 8 )
+print (numA + numB)
+
+#2. Resta
+
+print(43-81)
+print(numA-numB)
+
+#3. Multiplicación
+print (3*98)
+print (numA*numB)
+
+#4. División
+print(18/5)
+
+#5. Potencia
+print(2**3)
+
+#6. División entera
+print(18//5)
+
+#7. Operador Módulo (el resto de la división)
+print(18%5)
+
+#8. Incremento / Decremento / Asignación
+numero = 2 #asignación
+numero += 1 #numero = numero +2. Incremento y asginación
+numero -= 3 #numero = numero - 3. Decremento y asignación 
