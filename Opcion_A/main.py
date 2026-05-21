99ee31f54fe842c6d62885caa880c890c9f0ce42
LaraDelCerro <pececitodory@hotmail.com>
2026-05-21 13:56:24 +0200

    continuación práctica (limpieza de datos)

--- Opcion_A/main.py
+++ Opcion_A/main.py
@@ -1,5 +1,5 @@
-from lib.carga import cargar_excel, cargar_csv, cargar_json, cargar_xml
-from lib.auditoria import 
+from lib.carga import cargar_excel, cargar_csv, cargar_json, cargar_xml, pintar_info_fichero
+from lib.limpieza import limpiar_texto, normalizar_texto, procesar_csv, procesar_excel, procesar_json, procesar_xml
 
 horarios = cargar_excel('datos', 'escenarios_horarios.xlsx')
 #print(horarios)
@@ -12,7 +12,32 @@ ventas_entradas = cargar_json('datos', 'ventas_entradas.json')
 
 patrocinadores = cargar_xml('datos', 'patrocinadores.xml')
 
-print(patrocinadores)
+#print(patrocinadores)
 
 #print(len(horarios))
 #print(type(horarios))
+
+
+#print(normalizar_texto('  Juan garcia                lopez   '))
+
+
+
+#cabeceras =list(horarios[0].keys())
+#print(cabeceras)
+
+#pintar_info_fichero(horarios, 'escenarios_horarios.xlsx', 5)
+#pintar_info_fichero(artistas, 'artistas.csv', 5)
+#pintar_info_fichero(ventas_entradas, 'ventas_entradas.json', 5)
+#pintar_info_fichero(patrocinadores, 'patrocinadores.xml', 5)
+
+artistas_limpio = procesar_csv(artistas)
+#print(artistas_limpio)
+
+horarios_limpio = procesar_excel(horarios)
+#print(horarios_limpio)
+
+ventas_entradas_limpio = procesar_json(ventas_entradas)
+#print(ventas_entradas_limpio)
+
+patrocinadores_limpio = procesar_xml(patrocinadores)
+#print(patrocinadores_limpio)
