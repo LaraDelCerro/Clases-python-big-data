676985562d9b918f275c5af0c60d9746cf630c27
LaraDelCerro <pececitodory@hotmail.com>
2026-05-22 13:58:33 +0200

    continuación práctica

--- Opcion_A/main.py
+++ Opcion_A/main.py
@@ -1,5 +1,5 @@
 from lib.carga import cargar_excel, cargar_csv, cargar_json, cargar_xml, pintar_info_fichero
-from lib.limpieza import limpiar_texto, normalizar_texto, procesar_csv, procesar_excel, procesar_json, procesar_xml
+from lib.limpieza import limpiar_texto, normalizar_texto, procesar_csv, procesar_excel, procesar_json,procesar_xml, limpiar_valor_numerico
 
 horarios = cargar_excel('datos', 'escenarios_horarios.xlsx')
 #print(horarios)
@@ -33,11 +33,13 @@ patrocinadores = cargar_xml('datos', 'patrocinadores.xml')
 artistas_limpio = procesar_csv(artistas)
 #print(artistas_limpio)
 
-horarios_limpio = procesar_excel(horarios)
+#horarios_limpio = procesar_excel(horarios)
 #print(horarios_limpio)
 
-ventas_entradas_limpio = procesar_json(ventas_entradas)
+#ventas_entradas_limpio = procesar_json(ventas_entradas)
 #print(ventas_entradas_limpio)
 
-patrocinadores_limpio = procesar_xml(patrocinadores)
+#patrocinadores_limpio = procesar_xml(patrocinadores)
 #print(patrocinadores_limpio)
+
+
